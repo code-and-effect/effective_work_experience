7664cc552d5764b39d517523cda1334980226c90
Matt Riemer <matthew@codeandeffect.com>
2026-08-04 16:07:55 -0600

    Fix admin bug

--- db/migrate/101_create_effective_work_experience.rb
+++ db/migrate/101_create_effective_work_experience.rb
@@ -1,6 +1,6 @@
 class CreateEffectiveWorkExperience < ActiveRecord::Migration[6.0]
   def change
-    create_table :work_experience_categories do |t|
+    create_table :work_experience_categories, if_not_exists: true do |t|
       t.string :title
       t.integer :minimum_hours
       t.integer :position
@@ -8,7 +8,7 @@ class CreateEffectiveWorkExperience < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    create_table :work_experience_subcategories do |t|
+    create_table :work_experience_subcategories, if_not_exists: true do |t|
       t.integer :work_experience_category_id
 
       t.string :title
@@ -18,7 +18,7 @@ class CreateEffectiveWorkExperience < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    create_table :work_experience_outside_mentors do |t|
+    create_table :work_experience_outside_mentors, if_not_exists: true do |t|
       t.integer :user_id
       t.string :user_type
 
@@ -31,7 +31,7 @@ class CreateEffectiveWorkExperience < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    create_table :work_experience_records do |t|
+    create_table :work_experience_records, if_not_exists: true do |t|
       t.integer :user_id
       t.string :user_type
 
@@ -50,7 +50,7 @@ class CreateEffectiveWorkExperience < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    create_table :work_experience_entries do |t|
+    create_table :work_experience_entries, if_not_exists: true do |t|
       t.integer :user_id
       t.string :user_type
 
@@ -66,7 +66,7 @@ class CreateEffectiveWorkExperience < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    create_table :work_experience_projects do |t|
+    create_table :work_experience_projects, if_not_exists: true do |t|
       t.integer :user_id
       t.string :user_type
 
@@ -79,7 +79,7 @@ class CreateEffectiveWorkExperience < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    create_table :work_experience_summaries do |t|
+    create_table :work_experience_summaries, if_not_exists: true do |t|
       t.integer :user_id
       t.string :user_type
 
@@ -110,11 +110,11 @@ class CreateEffectiveWorkExperience < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    add_index :work_experience_outside_mentors, [:user_id, :user_type], unique: true
-    add_index :work_experience_records, [:user_id, :user_type]
-    add_index :work_experience_entries, [:work_experience_record_id]
-    add_index :work_experience_projects, [:user_id, :user_type]
-    add_index :work_experience_summaries, [:user_id, :user_type]
-    add_index :work_experience_summaries, [:mentor_id, :mentor_type]
+    add_index :work_experience_outside_mentors, [:user_id, :user_type], unique: true, if_not_exists: true
+    add_index :work_experience_records, [:user_id, :user_type], if_not_exists: true
+    add_index :work_experience_entries, [:work_experience_record_id], if_not_exists: true
+    add_index :work_experience_projects, [:user_id, :user_type], if_not_exists: true
+    add_index :work_experience_summaries, [:user_id, :user_type], if_not_exists: true
+    add_index :work_experience_summaries, [:mentor_id, :mentor_type], if_not_exists: true
   end
 end
